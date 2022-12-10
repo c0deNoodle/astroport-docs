883f3b2224e18a0e836f6e78a6501c69f1cd5583
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-12-10 18:46:44 +0000

    navbar swizzling

---
+++ src/theme/Navbar/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Navbar from '@theme-original/Navbar';
+
+import TwitterIcon from '../../components/icons/TwitterIcon';
+import DiscordIcon from '../../components/icons/DiscordIcon';
+import MediumIcon from '../../components/icons/MediumIcon';
+import TelegramIcon from '../../components/icons/TelegramIcon';
+import { APP_URL } from '../../constants';
+
+const NavigationItems = () => {
+  return (
+    <div className="flex gap-x-9">
+      <a>Blog</a>
+      <a>Community</a>
+      <a>Docs</a>
+      <a>Governance</a>
+    </div>
+  );
+};
+
+const iconsClassNames =
+  'h-7 hover:text-indigo-300 cursor-pointer transition-all duration-200';
+
+const SocialIcons = () => {
+  return (
+    <div className="flex gap-x-8 px-16">
+      <TwitterIcon className={iconsClassNames} />
+      <TelegramIcon className={iconsClassNames} />
+      <DiscordIcon className={iconsClassNames} />
+      <MediumIcon className={iconsClassNames} />
+    </div>
+  );
+};
+
+export default function NavbarWrapper(props) {
+  return (
+    <>
+      <div className="flex justify-end items-center h-[90px] px-6 md:px-[80px]">
+        <img
+          src="../../../img/logo.svg"
+          alt="logo"
+          className="h-[32px] mr-auto"
+        />
+        <NavigationItems />
+        <SocialIcons />
+        <button
+          className="uppercase rounded-2xl bg-primary py-2 px-6 text-[13px] font-medium text-white hover:bg-white hover:text-primary transition-all duration-500"
+          onClick={() => window.open(APP_URL, '_blank')}
+        >
+          Enter app
+        </button>
+      </div>
+      <Navbar {...props} />
+    </>
+  );
+}
