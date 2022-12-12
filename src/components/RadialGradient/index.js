9dff2ca4ca941d40df28a8a28f5f3b804494ac22
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-12-12 21:49:46 +0000

    remove obsolete componet

--- src/components/RadialGradient/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
-
-export default function RadialGradiant() {
-  return (
-    <div className={styles.gradiant}></div>
-  );
-}
