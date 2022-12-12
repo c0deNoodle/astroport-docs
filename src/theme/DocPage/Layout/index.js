324e8ec5fef06811695b39327e432b748350ecd4
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-12-12 22:07:26 +0000

    doc page layout swizzling

---
+++ src/theme/DocPage/Layout/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useDocsSidebar } from '@docusaurus/theme-common/internal';
+import Layout from '@theme/Layout';
+import BackToTopButton from '@theme/BackToTopButton';
+import DocPageLayoutSidebar from '@theme/DocPage/Layout/Sidebar';
+import DocPageLayoutMain from '@theme/DocPage/Layout/Main';
+import styles from './styles.module.css';
+
+import RadialGradient from '../../../components/RadialGradient';
+
+export default function DocPageLayout({ children }) {
+  const sidebar = useDocsSidebar();
+  const [hiddenSidebarContainer, setHiddenSidebarContainer] = useState(false);
+  return (
+    <Layout wrapperClassName={styles.docsWrapper}>
+      <RadialGradient />
+      <BackToTopButton />
+      <div className={styles.docPage}>
+        {sidebar && (
+          <DocPageLayoutSidebar
+            sidebar={sidebar.items}
+            hiddenSidebarContainer={hiddenSidebarContainer}
+            setHiddenSidebarContainer={setHiddenSidebarContainer}
+          />
+        )}
+        <DocPageLayoutMain hiddenSidebarContainer={hiddenSidebarContainer}>
+          {children}
+        </DocPageLayoutMain>
+      </div>
+    </Layout>
+  );
+}
