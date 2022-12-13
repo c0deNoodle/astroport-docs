fcd1f516eb03c94306140442f6978384e02b5374
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-12-13 17:49:25 +0000

    background and responsiveness improvements

--- src/components/RadialGradient.tsx
+++ src/components/RadialGradient.tsx
@@ -5,7 +5,7 @@ const RadialGradient = () => {
     <>
       {/* Radial Gradient */}
       <div
-        className="absolute z-0 w-[1500px] h-[1500px] left-1/2 ml-[-750px] top-[-150px]"
+        className="absolute z-0 w-full max-w-[1500px] h-full max-h-[1500px] left-1/2 -translate-x-1/2 top-[-150px]"
         style={{
           background:
             'radial-gradient(50% 50% at 50% 50%, #5643F2 0%, #83B3FD 58.33%, #2CA4D6 73.44%, #000D37 100%)',
