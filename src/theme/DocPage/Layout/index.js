d1b7a70e7b259882ee9164af564001882acdce2f
Dino De La O <dinodelao@proton.me>
2022-12-18 22:41:55 -0600

    doc page layout

--- src/theme/DocPage/Layout/index.js
+++ src/theme/DocPage/Layout/index.js
@@ -4,6 +4,7 @@ import Layout from '@theme/Layout';
 import BackToTopButton from '@theme/BackToTopButton';
 import DocPageLayoutSidebar from '@theme/DocPage/Layout/Sidebar';
 import DocPageLayoutMain from '@theme/DocPage/Layout/Main';
+import styles from './styles.module.css';
 
 import RadialGradient from '../../../components/RadialGradient';
 
@@ -11,10 +12,15 @@ export default function DocPageLayout({ children }) {
   const sidebar = useDocsSidebar();
   const [hiddenSidebarContainer, setHiddenSidebarContainer] = useState(false);
   return (
-    <Layout wrapperClassName="flex">
+    <Layout wrapperClassName={styles.docsWrapper}>
       <RadialGradient />
       <BackToTopButton />
-      <div className="relative w-full flex px-0">
+      <div className={styles.docPage}>
+
+      <DocPageLayoutMain hiddenSidebarContainer={hiddenSidebarContainer}>
+
+
+      <div className={styles.container}>
         {sidebar && (
           <DocPageLayoutSidebar
             sidebar={sidebar.items}
@@ -22,10 +28,101 @@ export default function DocPageLayout({ children }) {
             setHiddenSidebarContainer={setHiddenSidebarContainer}
           />
         )}
-        <DocPageLayoutMain hiddenSidebarContainer={hiddenSidebarContainer}>
+
+
           {children}
+        </div>
+
         </DocPageLayoutMain>
       </div>
     </Layout>
   );
 }
+
+
+
+// import React, {useState} from 'react';
+// import {useDocsSidebar} from '@docusaurus/theme-common/internal';
+// import Layout from '@theme/Layout';
+// import BackToTopButton from '@theme/BackToTopButton';
+// import DocPageLayoutSidebar from '@theme/DocPage/Layout/Sidebar';
+// import DocPageLayoutMain from '@theme/DocPage/Layout/Main';
+// import styles from './styles.module.css';
+// import {useEffect, useRef} from 'react';
+
+
+// // import OverviewBannerUrl from '../../../../static/img/docspage-banners/hero-banner.png';
+
+// export default function DocPageLayout({children}) {
+//   const sidebar = useDocsSidebar();
+//   const [hiddenSidebarContainer, setHiddenSidebarContainer] = useState(false);
+//   const ref = useRef(null);
+
+  // if ("plugin-id-overview" == true) {
+
+  // useEffect(() => {
+  //   console.log('plugin-id-overview', ref.current.className);
+
+  //   // 👇️ check if element contains class
+  //   if (ref.current.classList.contains('plugin-id-overview')) {
+  //     console.log('Element contains class');
+  //   } else {
+  //     console.log('Element does NOT contain class');
+  //   }
+  // }, []);
+
+  // const handleClick = event => {
+  //   console.log(event.currentTarget.className);
+
+  //   // 👇️ check if element contains class
+  //   if (event.currentTarget.classList.contains('plugin-id-overview')) {
+  //     console.log('Element contains overview');
+  //   } else if (event.currentTarget.classList.contains('plugin-id-learn')) {
+  //     console.log('Element contains learn');
+  //   } else {
+  //     console.log('Element does NOT contain class');
+  //   }
+  // };
+
+//       return (
+//         <Layout wrapperClassName={styles.docsWrapper}>
+    
+//           {/* <div className={styles.banner}></div> */}
+    
+//           {/* <div>
+//             <div ref={ref} onClick={handleClick}>
+//               Hello world
+//             </div>
+//           </div> */}
+
+//           {/* <img src={OverviewBannerUrl} className={styles.banner} /> */}
+    
+//           <BackToTopButton />
+//           <div className={styles.docPage}>
+    
+//             <DocPageLayoutMain hiddenSidebarContainer={hiddenSidebarContainer}>
+    
+//             <div className={styles.container}>
+
+//               {sidebar && (
+//                 <DocPageLayoutSidebar
+//                   sidebar={sidebar.items}
+//                   hiddenSidebarContainer={hiddenSidebarContainer}
+//                   setHiddenSidebarContainer={setHiddenSidebarContainer}
+//                 />
+//               )}
+
+//               {children}
+//               </div>
+    
+//             </DocPageLayoutMain>
+    
+//           </div>
+    
+    
+//         </Layout>
+//       );
+//     // } 
+// }
+
+  
