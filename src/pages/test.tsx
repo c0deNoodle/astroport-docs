22324591d2a2703368e706017d5b20659344adff
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2023-01-12 12:49:04 +0000

    removed commented code

---
+++ src/pages/test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+// import Header from '../components/Header';
+
+const Test = () => {
+  return (
+    <>
+      {/* <Header /> */}
+      <Layout>
+        <div>Test Page Content</div>
+      </Layout>
+    </>
+  );
+};
+
+export default Test;
