616588213fc45a144f5149905b64837cf5a5e81e
Dino De La O <dinodelao@proton.me>
2023-03-24 11:10:42 +0100

    replace react-dom/client with render

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './app'
 // import './index.css'
 import { KeplrProvider, Network, ShuttleProvider } from "@delphi-labs/shuttle";
